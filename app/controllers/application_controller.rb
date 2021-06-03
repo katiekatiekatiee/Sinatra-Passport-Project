c1076a0409ede99be94695604a0acab2a3d01a36
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-06-03 18:24:09 -0400

    helpers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,35 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
-  get "/signup" do
-    erb :"/users/signup.html"
-  end
-
-  post "/signup" do
-    user = User.new(params)
-    if user.save
-      session[:user_id] = user.id
-      redirect :"/countries"
+  helpers do
+    def current_user
+      @current_user ||= User.find(id: session[:user_id])
     end
-    redirect :"/signup"
-  end
 
-  get "/login" do
-    erb :"/users/login.html"
-  end
-
-  post "/login" do 
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect "/countries"
+    def logged_in?
+      !!current_user
     end
-    redirect "/login"
-  end
 
-  post "/logout" do
-    session.clear
-    redirect "/countries"
   end
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,36 @@ class UsersController < ApplicationController
   delete "/users/:id/delete" do
     redirect "/users"
   end
+
+  get "/signup" do
+    erb :"/users/signup.html"
+  end
+
+  post "/signup" do
+    user = User.new(params)
+    if user.save
+      session[:user_id] = user.id
+      redirect :"/countries"
+    end
+    redirect :"/signup"
+  end
+
+  get "/login" do
+    erb :"/users/login.html"
+  end
+
+  post "/login" do 
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/countries"
+    end
+    redirect "/login"
+  end
+
+  post "/logout" do
+    session.clear
+    redirect "/countries"
+  end
+  
 end
