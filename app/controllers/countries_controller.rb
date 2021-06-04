ba66c96aa273ab6e35fe34811e9930a1c4deefb9
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-06-04 19:28:52 -0400

    redirect if not logged in added

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,11 +7,13 @@ class CountriesController < ApplicationController
   end
 
   get "/countries/new" do
+    redirect_if_not_logged_in 
     @countries = Country.all
     erb :"/countries/new.html"
   end
 
   post "/countries" do
+    redirect_if_not_logged_in 
     @country = Country.new(params)
     @country.user_id = session[:user_id]
     @country.save
@@ -19,19 +21,29 @@ class CountriesController < ApplicationController
   end
 
   get "/countries/:id" do
+    redirect_if_not_logged_in 
     @country = Country.find_by(id: params[:id])
     erb :"/countries/show.html"
   end
 
   get "/countries/:id/edit" do
+    redirect_if_not_logged_in 
     erb :"/countries/edit.html"
   end
 
   patch "/countries/:id" do
+    redirect_if_not_logged_in 
     redirect "/countries/:id"
   end
 
   delete "/countries/:id/delete" do
     redirect "/countries"
   end
+
+  private 
+    def redirect_if_not_logged_in
+      if @country.user != current_user 
+        redirect '/countries'
+      end
+    end
 end
