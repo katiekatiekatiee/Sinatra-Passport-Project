d181ee864012fe038f83a793059f260c3f18ac88
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-06-14 11:57:42 -0400

    add log in and sign up to log in and sign up pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
     user = User.new(params)
     if user.save
       session[:user_id] = user.id
-      redirect :"/countries"
+      redirect "/countries"
     end
-    redirect :"/signup"
+    redirect "/signup"
   end
 
   get "/login" do
